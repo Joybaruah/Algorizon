de3ee7646c2142a1b38bd7ef9e3515c929007de1
Joybaruah <hoyyun1@gmail.com>
2022-07-08 03:06:21 -0700

    c

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { BiMenuAltRight } from "react-icons/bi";
-import { Link , useLocation} from "react-router-dom";
-import { motion } from "framer-motion"
+import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import { GrClose } from "react-icons/gr";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
@@ -18,30 +19,44 @@ const Navbar = () => {
 
   return (
     <div>
-      <div className="flex sm:hidden justify-end bg-violet-500/60 w-full fixed">
-        <BiMenuAltRight size={25} className="m-1" onClick={handleClick} />
+      <div className="flex sm:hidden justify-end bg-violet-500/60 w-full fixed p-2">
+        <BiMenuAltRight size={30}  onClick={handleClick} />
       </div>
 
       <div
         className={`${
           active ? "block" : "hidden"
-        } backdrop-blur-sm absolute w-[100vw] h-[100vh]`}
+        } backdrop-blur-sm absolute w-[100vw] h-[100vh] fixed`}
       >
+        <div className="flex sm:hidden justify-end w-full fixed">
+          <GrClose size={25} className="m-3" onClick={handleClick} />
+        </div>
         <ul className="flex ml-3 gap-4 mt-4 flex-col">
-          <Link to={'/'} className="font-Poppins font-semibold">HOME</Link>
-          <Link to={'/company'} className="font-Poppins font-semibold">COMPANY PROFILE</Link>
+          <Link to={"/"} className="font-Poppins font-semibold mt-6">
+            HOME
+          </Link>
+          <Link to={"/company"} className="font-Poppins font-semibold">
+            COMPANY PROFILE
+          </Link>
           <li className="font-Poppins font-semibold">SERVICES</li>
           <li className="font-Poppins font-semibold">PRODUCTS</li>
           <li className="font-Poppins font-semibold">CONTACT</li>
         </ul>
       </div>
 
-      <motion.nav
-      className="hidden sm:flex justify-end ">
+      <motion.nav className="hidden sm:flex justify-end ">
         <div className="fixed">
           <ul className="flex gap-4 mt-3 flex-row bg-violet-500/30 py-4 px-5">
-            <Link to={'/'} className="font-Poppins font-semibold cursor-pointer">HOME</Link>
-            <Link to={'/company'} className="font-Poppins font-semibold cursor-pointer">
+            <Link
+              to={"/"}
+              className="font-Poppins font-semibold cursor-pointer"
+            >
+              HOME
+            </Link>
+            <Link
+              to={"/company"}
+              className="font-Poppins font-semibold cursor-pointer"
+            >
               COMPANY PROFILE
             </Link>
             <li className="font-Poppins font-semibold cursor-pointer">
