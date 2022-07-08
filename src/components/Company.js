64282d28f71795ecf16f2044afea7e9f0513c397
Joybaruah <hoyyun1@gmail.com>
2022-07-08 05:13:08 -0700

    c

--- src/components/Company.js
+++ src/components/Company.js
@@ -3,10 +3,10 @@ import React from "react";
 const Company = () => {
   return (
     <>
-      <div className="bg-gradient-to-r from-purple-300 via-purple-200 to-purple-100 h-[100vh] shadow-sm">
-        <div className="pt-24 sm:pt-28 md:pt-40 lg:pt-52 flex justify-center">
+      <div className="bg-gradient-to-r from-purple-300 via-purple-200 to-purple-100 h-[60vh] lg:h-[100vh] shadow-sm">
+        <div className="flex items-center justify-center  h-[60vh] lg:h-[100vh] ">
           <h2 className=" font-Poppins text-2xl text-violet-700 font-extrabold sm:text-6xl">
-            Company Profile
+            COMPANY PROFILE
           </h2>
         </div>
       </div>
